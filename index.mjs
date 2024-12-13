7d4f1c96131966a08c193a89bde7aa2de9b0a41d
Etativel <farhanmaulana4611@gmail.com>
2024-12-13 10:39:12 +0800

    feat: serve each pages according to the query

---
+++ index.mjs
@@ -0,0 +1,28 @@
+import http from "http";
+import url from "url";
+import fs from "fs";
+
+http
+  .createServer((req, res) => {
+    let query = url.parse(req.url, true);
+    let filePath =
+      "./pages/" +
+      (query.pathname === "/" ? "/index.html" : query.pathname + ".html");
+    let errorPath = "./pages/404.html";
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        fs.readFile(errorPath, (err, data) => {
+          if (err) {
+            res.writeHead(500, { "Content-Type": "text/html" });
+            return res.end("<h1>500 Internal Server Error</h1>");
+          }
+          res.writeHead(404, { "Content-Type": "text/html" });
+          return res.end(data);
+        });
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      return res.end(data);
+    });
+  })
+  .listen(8080);
